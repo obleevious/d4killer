e709cbeed2b3b0b8b825b37362dc3eb0dca66311
Hanrun Li <lihanrun@outlook.com>
2023-07-24 22:35:21 -0700

    Switch to easyocr jaidedAI

--- filter_items.py
+++ filter_items.py
@@ -2,14 +2,17 @@
 import sys
 import numpy as nm
 import time
-import pytesseract
 import ctypes
 import pyautogui
 # importing OpenCV
 import cv2
+import easyocr
 
 from PIL import ImageGrab
 from pynput import keyboard
+
+# reader = easyocr.Reader(['ch_sim','en'])
+reader = easyocr.Reader(['ch_sim'])
 
 TESSEARACT_ROOT = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 MOVE_DURATION = 0.2
@@ -17,32 +20,20 @@ ROW_NUM = 3
 COL_NUM = 11
 ORC_LANG = 'chi_sim'
 
-# Path of tesseract executable
-pytesseract.pytesseract.tesseract_cmd = TESSEARACT_ROOT
-
 def get_current_position(a, b):
     i, j, x, y = get_start_end_point()
     return i + ((x-i)/(COL_NUM-1) * a), j + ((y-j)/(ROW_NUM-1) * b)
 
-def isGoodItem():
+def isGoodItem(item_details):
+    print(item_details)
     return False
 
 def get_item_details(a, b):
-    cap = ImageGrab.grab(bbox =(605, 215, 855, 675)) 
+    cap = ImageGrab.grab(bbox =(605, 215, 855, 675))
     # cap = ImageGrab.grab(bbox =(605, 265, 855, 295))
-
-    # print(nm.array(cap))
-    
-    # cv2.imshow("window_name", cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY))
-
-    # imToString(cap)
-
-    tesstr = pytesseract.image_to_string(
-        cap, 
-        lang = ORC_LANG)
-    
-    print(tesstr)
     cap.show()
+    result = reader.readtext(cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY), detail = 0)
+    return result
 
 def filter_current_item(a, b):
     x, y = get_current_position(a, b)
@@ -50,7 +41,7 @@ def filter_current_item(a, b):
     pyautogui.moveTo(x, y, duration = MOVE_DURATION)
     time.sleep(MOVE_DURATION)
     item_details = get_item_details(a, b)
-    if not isGoodItem():
+    if not isGoodItem(item_details):
         pyautogui.keyDown("space")
 
 def filter_items():
@@ -67,13 +58,6 @@ def get_start_end_point():
     print(x, y)
     return (x * 0.34375, y * 0.36111, x * 0.4854, y * 0.4305)
 
-def imToString(cap):
-    print(pytesseract.get_languages())
-    tesstr = pytesseract.image_to_string(
-            cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY), 
-            lang = ORC_LANG)
-    print(tesstr)
-  
 def on_press(key):
     if key == keyboard.Key.esc:
         sys.exit()
@@ -83,7 +67,7 @@ def on_press(key):
         k = key.name  # other keys
     if k == 'backspace':
         filter_items()
-        # return False  
+        # return False
 
 def start_listener():
     listener = keyboard.Listener(on_press=on_press)
